713833c5c06882ac7acec6dad1e3b171eb0956d1
bruno <brunofvteixeira@gmail.com>
2022-10-24 19:15:42 -0300

    evaluate return statements

--- object/object.go
+++ object/object.go
@@ -8,6 +8,7 @@ const (
   INTEGER_OBJ = "INTEGER"
   BOOLEAN_OBJ = "BOOLEAN"
   NULL_OBJ = "NULL"
+  RETURN_VALUE_OBJ = "RETURN_VALUE"
 )
 
 type Object interface {
@@ -33,4 +34,11 @@ type Null struct {}
 
 func (b *Null) Type() ObjectType{ return NULL_OBJ }
 func (b *Null) Inspect() string { return "null" }
+
+type ReturnValue struct {
+  Value Object
+}
+
+func (rv *ReturnValue) Type() ObjectType { return RETURN_VALUE_OBJ }
+func (rv *ReturnValue) Inspect() string { return rv.Value.Inspect() }
 
