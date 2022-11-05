185e9b340996beed61fc603bbb88a11ceffd30da
bruno <brunofvteixeira@gmail.com>
2022-11-05 19:41:39 -0300

    array len, first, push

--- evaluator/builtins.go
+++ evaluator/builtins.go
@@ -10,6 +10,8 @@ var builtins = map[string]*object.Builtin{
 					len(args))
 			}
 			switch arg := args[0].(type) {
+			case *object.Array:
+				return &object.Integer{Value: int64(len(arg.Elements))}
 			case *object.String:
 				return &object.Integer{Value: int64(len(arg.Value))}
 			default:
@@ -17,4 +19,50 @@ var builtins = map[string]*object.Builtin{
 			}
 		},
 	},
+	"first": {
+		Fn: func(args ...object.Object) object.Object {
+			if len(args) != 1 {
+				return newError("wrong number of arguments. got=%d, want=1", len(args))
+			}
+			arr := args[0].(*object.Array)
+			if len(arr.Elements) > 0 {
+				return arr.Elements[0]
+			}
+
+			return NULL
+		},
+	},
+	"rest": {
+		Fn: func(args ...object.Object) object.Object {
+			if len(args) != 1 {
+				return newError("wrong number of arguments. got=%d, want=1", len(args))
+			}
+
+			arr := args[0].(*object.Array)
+			length := len(arr.Elements)
+			if length > 0 {
+				newElements := make([]object.Object, length-1, length-1)
+        copy(newElements, arr.Elements[1:length])
+        return &object.Array{Elements: newElements}
+			}
+
+			return NULL
+		},
+	},
+  "push": {
+    Fn: func(args ...object.Object) object.Object {
+      if len(args) != 2 {
+        return newError("wrong number of arguments. got=%d, want=2", len(args))
+      }
+
+      arr := args[0].(*object.Array)
+      length := len(arr.Elements)
+
+      newElements := make([]object.Object, length+1, length+1)
+      copy(newElements, arr.Elements)
+      newElements[length] = args[1]
+
+      return &object.Array{Elements: newElements}
+    },
+  },
 }
